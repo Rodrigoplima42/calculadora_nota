7ee1c4bbd6ce17abd924b7710035bf593890e76b
Rodrigoplima42 <pavan.lima@gmail.com>
2022-01-15 09:06:20 -0300

    feito rotinas de erros

--- nota.py
+++ nota.py
@@ -2,12 +2,22 @@ import sys
 
 
 def error():
-    print('Voc^deve passar a nota como argumento do comendo')
+    print('Você deve passar a nota como argumento do comendo')
     print('formato: nota.py <<nota>>')
+
+
+def error_numeric():
+    print('o valor fornecido como argumento deve ser um numero')
+    print('formato: nota.py <<nota>>')
+
 
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         error()
+    elif not sys.argv[1].isnumeric():
+        error_numeric()
+print(sys.argv[1].isnumeric())
+
     #else:
         # calcular_nota(sys.argv[1])
